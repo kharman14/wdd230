fcf57adb1ff1e7c30e88fb4ba2645a458b7b9b45
kharman14 <harman.katelyn96@gmail.com>
2022-04-23 16:20:42 -0600

    Submission Week 1

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
 
 const currentDateandTime = new Date();
 const copyrightyear = document.querySelector('#copyrightyear');
-const oLastModif = document.querySelector('#lastmodified');
 
 copyrightyear.textContent = currentDateandTime.getFullYear();
 
-oLastModif.textContent = currentDateandTime.lastmodified();
-
-
+let lastModif = new Date(document.lastModified);
+let fullDate = lastModif.toLocaleString('en-US', {month: "2-digit", day: "2-digit", year: "numeric"});
+let time = lastModif.toLocaleString('en-GB', {hour: "2-digit", minute: "2-digit", second: "2-digit"});
+let dateTime = `Last Updated: ${fullDate} ${time}`;
+document.getElementById("lastmodified").innerHTML = dateTime;
