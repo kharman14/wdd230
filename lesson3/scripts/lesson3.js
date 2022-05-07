1852f40652f4b4b1999ab504c4a90df2b3538244
kharman14 <harman.katelyn96@gmail.com>
2022-05-06 22:33:22 -0600

    Work on lesson 3 Chamber of commerce site

---
+++ lesson3/scripts/lesson3.js
@@ -0,0 +1,11 @@
+
+const currentDateandTime = new Date();
+const copyrightyear = document.querySelector('#copyrightyear');
+
+copyrightyear.textContent = currentDateandTime.getFullYear();
+
+let lastModif = new Date(document.lastModified);
+let fullDate = lastModif.toLocaleString('en-US', {month: "2-digit", day: "2-digit", year: "numeric"});
+let time = lastModif.toLocaleString('en-GB', {hour: "2-digit", minute: "2-digit", second: "2-digit"});
+let dateTime = `Last Updated: ${fullDate} ${time}`;
+document.getElementById("lastmodified").innerHTML = dateTime;
