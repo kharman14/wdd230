ca2bf0873af331c7bab81959876856b1270847f8
kharman14 <harman.katelyn96@gmail.com>
2022-05-21 17:15:12 -0600

    Make banner and resize an image

--- chamber/js/chamber.js
+++ chamber/js/chamber.js
@@ -1,3 +1,4 @@
+// Copyright Year
 const currentDateandTime = new Date();
 const copyrightyear = document.querySelector('#copyrightyear');
 
@@ -6,8 +7,27 @@ document.querySelector("#currentDate").innerHTML =
 
 copyrightyear.textContent = currentDateandTime.getFullYear();
 
+// Last Modified
 let lastModif = new Date(document.lastModified);
 let fullDate = lastModif.toLocaleString('en-US', {month: "2-digit", day: "2-digit", year: "numeric"});
 let time = lastModif.toLocaleString('en-GB', {hour: "2-digit", minute: "2-digit", second: "2-digit"});
 let dateTime = `Last Updated: ${fullDate} ${time}`;
 document.getElementById("lastmodified").innerHTML = dateTime;
+
+// Banner
+function invite() {
+    let banner = document.getElementById('banner');
+
+    date = new Date();
+    dayOfWeek = date.getDay();
+
+    if (dayOfWeek == 1 || dayOfWeek == 2) {
+        banner.style.display = 'block';
+    } else {
+        banner.style.display = 'none';
+    }
+
+    console.log(dayOfWeek);
+};
+
+invite();
