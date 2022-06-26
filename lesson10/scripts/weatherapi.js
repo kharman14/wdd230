7f81b2c717525407edf95f17491840359a454bf8
kharman14 <harman.katelyn96@gmail.com>
2022-06-25 23:14:11 -0600

    Weather API activity

---
+++ lesson10/scripts/weatherapi.js
@@ -0,0 +1,43 @@
+//api_key = 12240cde702580291dd8baa5ce236efa;
+
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+const url = `https://api.openweathermap.org/data/2.5/weather?q=Fairbanks&units=imperial&appid=12240cde702580291dd8baa5ce236efa`;
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data);
+            displayResults(data);
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+apiFetch();
+
+function displayResults(weatherData) {
+    currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
+
+    const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+    const desc = weatherData.weather[0].description;
+
+    // to capitalize each word in desc
+    const str = desc.split(" ");
+    for (var i = 0; i < str.length; i++) {
+        str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1);
+    }
+
+    const new_desc = str.join(" ");
+
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', new_desc);
+    captionDesc.textContent = new_desc;
+}
